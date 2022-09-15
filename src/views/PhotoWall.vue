614f7c1473affff6e59d493bbd1271685da701d7
fwio.fu@ebaotech.com <624835339@qq.com>
2022-09-15 15:27:42 +0800

    Rewrite the photo wall with grid layout.

--- src/views/PhotoWall.vue
+++ src/views/PhotoWall.vue
@@ -3,101 +3,85 @@
 </script>
 
 <template>
-  <div class="wall-wrapper">
-    <div
-      class="photo-wrapper"
-      v-for="n in 8">
-    </div>
+  <div class="grid-container">
+    <div v-for="n in 8" :key="n" class="grid-item"  />
   </div>
 </template>
 
 <style scoped lang="scss">
 $count: 8;
 
-.wall-wrapper {
+.grid-container {
   display: grid;
-  background-color: rgb(255, 255, 255, .8);
-  font-size: 1rem;
-  padding: .8em;
-  border-radius: .3em .4em;
   grid-template-columns: repeat(4, 1fr);
   grid-template-rows: repeat(8, 1fr);
+  gap: .5em;
+  
+  background-color: rgba(255, 255, 255, .8);
+  padding: .8em;
+  border-radius: .6em;
   height: 30em;
   width: 40em;
+  box-sizing: border-box;
 
   &::before {
-    object-fit: scale-down;
     content: "";
     position: absolute;
     top: 0;
     left: 0;
-    bottom: 0;
     right: 0;
-    z-index: 0;
+    bottom: 0;
+    border-radius: .6em;
+
+    z-index: -1;
     backdrop-filter: blur(4px);
   }
 }
 
-@for $i from 1 to $count+1 {
-  .photo-wrapper:nth-child(#{$i}) {
-    position: relative;
-    margin: 0.5vw;
+.grid-item {
+  background-position: center;
+  background-size: cover;
 
-    &::after {
-      content: "#{$i}";
-      position: absolute;
-      color: #fff;
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%, -50%);
-    }
-  }
-}
-
-.photo-wrapper {
   &:nth-child(1) {
     grid-column: 1;
     grid-row: 1 / 3;
-    background-image: url('../../images/1.jpg');
+    background-image: url(/images/1.jpg);
   }
   &:nth-child(2) {
     grid-column: 2;
     grid-row: 1 / 4;
-    background-image: url('../../images/2.jpg');
-    background-position: center;    
+    background-image: url(/images/2.jpg);
   }
   &:nth-child(3) {
     grid-column: 3;
     grid-row: 1 / 5;
-    background-image: url('../../images/3.jpg');    
+    background-image: url(/images/3.jpg);
   }
   &:nth-child(4) {
     grid-column: 4;
     grid-row: 1 / 6;
-    background-image: url('../../images/4.jpg');
-    background-position: center; 
+    background-image: url(/images/4.jpg);
   }
   &:nth-child(5) {
     grid-column: 1;
+    /* end-line is not contained */
     grid-row: 3 / 9;
-    background-image: url('../../images/5.jpg');    
+    background-image: url(/images/5.jpg);
   }
   &:nth-child(6) {
     grid-column: 2;
     grid-row: 4 / 9;
-    background-image: url('../../images/6.jpg');    
+    background-image: url(/images/6.jpg);
   }
   &:nth-child(7) {
     grid-column: 3;
     grid-row: 5 / 9;
-    background-image: url('../../images/7.jpg');    
+    background-image: url(/images/7.jpg);
   }
   &:nth-child(8) {
     grid-column: 4;
     grid-row: 6 / 9;
-    background-image: url('../../images/8.jpg');
-    background-position: center;
-    background-size: contain;
+    background-image: url(/images/8.jpg);
   }  
 }
 </style>
